a7427f9209f09de2d024a5553ac795dbb438dbea
dominicwild <dominicwild@dominicwild.com>
2023-07-15 15:33:35 +0100

    Basic first test and added acceptance test

--- src/dateCalculator.test.ts
+++ src/dateCalculator.test.ts
@@ -1,7 +1,57 @@
-describe('abc', function () {
+import dateCalculator, { DateOperation } from "./dateCalculator";
 
-  it("works", () => {
-    const a = "abc";
+const now = new Date("2020-05-01");
+jest
+    .useFakeTimers()
+    .setSystemTime(now);
+
+
+describe('Date Calculator Tests', function () {
+
+  describe("Acceptance Test", () => {
+    it.each<[DateOperation, string]>([
+      ["now-1y/y", "2019-01-01T00:00:00.000Z"],
+      ["now/y", "2020-01-01T00:00:00.000Z"],
+      ["now-1d", "2020-04-30T00:00:00.000Z"],
+      ["now+1d", "2020-05-02T00:00:00.000Z"],
+      ["now-4d-4h", "2020-04-26T20:00:00.000Z"],
+    ])
+    ("Operation %s gives date %s", (opString, expectedDateString) => {
+      let date = dateCalculator.parse(opString);
+      const expectedDate = new Date(Date.parse(expectedDateString));
+      expect(date).toBe(expectedDate)
+    })
+
+    it.each<[string, DateOperation]>([
+      ["2019-01-01T00:00:00.000Z", "now-1y/y"],
+      ["2020-01-01T00:00:00.000Z", "now/y"],
+      ["2020-04-30T00:00:00.000Z", "now-1d"],
+      ["2020-05-02T00:00:00.000Z", "now+1d"],
+      ["2020-04-26T20:00:00.000Z", "now-4d-4h"],
+    ])
+    ("Stringify %s gives operation %s", (dateString, expectedOp) => {
+      const dateInput = new Date(Date.parse(dateString));
+      let operation = dateCalculator.stringify(dateInput);
+      expect(operation).toBe(expectedOp)
+    })
   })
+
+  it.each<[DateOperation, string]>([
+    ["now", "2020-05-01T00:00:00.000Z"],
+  ])
+  ("Operation %s gives date %s", (opString: DateOperation, expectedDateString) => {
+    const date = dateCalculator.parse(opString);
+    const expectedDate = new Date(Date.parse(expectedDateString));
+    expect(date).toEqual(expectedDate)
+  })
+
+  // it.each([
+  //   ["2019-01-01T00:00:00.000Z", "now-1y/y"],
+  // ])
+  // ("Stringify %s gives operation %s", (dateString, expectedOp) => {
+  //   const dateInput = new Date(Date.parse(dateString));
+  //   let operation = dateCalculator.stringify(dateInput);
+  //   expect(operation).toBe(expectedOp)
+  // })
 
 });
--- src/dateCalculator.ts
+++ src/dateCalculator.ts
@@ -1,6 +1,21 @@
-module.exports = {
-  presets: [
-    ['@babel/preset-env', {targets: {node: 'current'}}],
-    '@babel/preset-typescript',
-  ],
-};
+export type DateOperation = "now" | `now${string}`;
+
+
+type DateFunctions = {
+  parse(dateString: DateOperation): Date
+  stringify(date: Date): DateOperation
+}
+
+class DateCalculator implements DateFunctions {
+
+  parse(dateString: DateOperation): Date {
+    return new Date(Date.now());
+  }
+
+  stringify(date: Date): DateOperation {
+    return "now-testOp";
+  }
+
+}
+
+export default new DateCalculator();
